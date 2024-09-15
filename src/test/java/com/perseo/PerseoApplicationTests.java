fc5d6ac2ed7333a0d9a6f1f1c19645bfd6c980ed
anamartin99 <anamariamartinherrera4@gmail.com>
2024-09-15 14:58:35 +0200

    first commit

---
+++ src/test/java/com/perseo/PerseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
